2b42afcb71f3dacc18157f363038b4da8c46d2b8
Jaime <jaalorsa517@hotmail.com>
2021-01-10 01:43:39 -0500

    Se cambió el título de la página

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   transpileDependencies: [],
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "CV Jaime Ortiz";
+      return args;
+    });
+  },
 };
