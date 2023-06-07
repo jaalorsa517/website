1e8d53f3b617c8f85c28140533bc82a2ae4baa00
jaalorsa517 <jaalorsa517@hotmail.com>
2023-06-06 22:20:43 -0500

    Corrigiendo vulnerabilidades. Instalando GA firebase

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { useRoot } from "@/store/index";
-import { j5JsonTransform, j5Carousel, j5MenuHamburguer, j5Toggle, j5Tooltip, j5Collapse } from "@jaalorsa/j5-components";
+import { j5JsonTransform, j5Carousel, j5MenuHamburguer } from "@jaalorsa/j5-components";
+
+function initFirebase() {
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  };
+  const firebaseApp = initializeApp(firebaseConfig);
+  getAnalytics(firebaseApp);
+}
 
 export function init() {
+  initFirebase();
   j5JsonTransform();
   j5Carousel();
   j5MenuHamburguer();
