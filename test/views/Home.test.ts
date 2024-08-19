a5ee051a6819d75d6a7524c25b2085ff6a0c21b8
jaalorsa517 <jaalorsa517@hotmail.com>
2024-08-19 13:39:01 -0500

    Test view Home

---
+++ test/views/Home.test.ts
@@ -0,0 +1,12 @@
+import Home from "@/views/Home.vue";
+import { describe, it, expect } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+
+describe("Home component", () => {
+
+  it("El componente Home renderiza correctamente", () => {
+    const wrapper = shallowMount(Home);
+    expect(wrapper.html()).toBeTruthy();
+    wrapper.unmount();
+  });
+})
