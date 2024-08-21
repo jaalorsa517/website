10e855aef677f4d477d6f8ce489c057f5a97e6e7
jaalorsa517 <jaalorsa517@hotmail.com>
2024-08-21 13:22:02 -0500

    Ajustando importacion routes

--- src/plugins/router/index.ts
+++ src/plugins/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import routes from "./routes";
+import { routes } from "./routes";
 import { seo } from "@/assets/resources/seo";
 import { inject } from "vue";
 import { SeoServiceInject } from "@/shared/constants/injectsKey";
