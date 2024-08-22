017febc00b8edd227303eecfe488d181e709876d
jaalorsa517 <jaalorsa517@hotmail.com>
2024-08-21 20:15:49 -0500

    Actualizando Tests y agregando badge

--- test/views/Home.test.ts
+++ test/views/Home.test.ts
@@ -3,10 +3,11 @@ import { describe, it, expect } from "vitest";
 import { shallowMount } from "@vue/test-utils";
 
 describe("Home component", () => {
-
   it("El componente Home renderiza correctamente", () => {
-    const wrapper = shallowMount(Home);
+    const wrapper = shallowMount(Home, {
+      stubs: ["Separate"],
+    });
     expect(wrapper.html()).toBeTruthy();
     wrapper.unmount();
   });
-})
+});
